52ff92febae27b70e99f45a564bc6d2ed3e3cc54
Khairil Yusof <khairil.yusof@gmail.com>
2021-06-03 10:45:00 +0800

    "Add behavior: ExtractiveResources"

---
+++ src/politikus/extractives/tests/test_behavior_extractive_resources.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from politikus.extractives.behaviors.extractive_resources import IExtractiveResourcesMarker
+from politikus.extractives.testing import POLITIKUS_EXTRACTIVES_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.behavior.interfaces import IBehavior
+from zope.component import getUtility
+
+import unittest
+
+
+class ExtractiveResourcesIntegrationTest(unittest.TestCase):
+
+    layer = POLITIKUS_EXTRACTIVES_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_behavior_extractive_resources(self):
+        behavior = getUtility(IBehavior, 'politikus.extractives.extractive_resources')
+        self.assertEqual(
+            behavior.marker,
+            IExtractiveResourcesMarker,
+        )
