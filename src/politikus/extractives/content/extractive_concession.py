8e32677d55a961a455d6790e59883f83456ac8cb
Khairil Yusof <khairil.yusof@gmail.com>
2021-06-03 09:42:34 +0800

    Initial Extractive Concession type

--- src/politikus/extractives/content/extractive_concession.py
+++ src/politikus/extractives/content/extractive_concession.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 # from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.autoform import directives
 from plone.dexterity.content import Container
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from plone.app.z3cform.widget import SelectFieldWidget
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from z3c.relationfield.schema import RelationChoice
+from z3c.relationfield.schema import RelationList
+from plone.app.vocabularies.catalog import CatalogSource
+from zope import schema
+from collective import dexteritytextindexer
 from zope.interface import implementer
 
-
-# from politikus.extractives import _
+from politikus.extractives import _
 
 
 class IExtractiveConcession(model.Schema):
@@ -20,6 +25,125 @@ class IExtractiveConcession(model.Schema):
     # and customize it in Python:
 
     # model.load('extractive_concession.xml')
+
+    dexteritytextindexer.searchable('title')
+    title = schema.TextLine(
+                title=_(u'Title'),
+                description=_(u'''
+                Concession Title
+                '''),
+                required=True,
+                )
+
+    dexteritytextindexer.searchable('description')
+    description = schema.Text(
+                title=_(u'Description'),
+                description=_(u'''
+                Concession description
+                '''),
+                required=False,
+                )
+
+    # Award Status
+    directives.widget(award_status=SelectFieldWidget)
+    award_status = schema.List(
+                title=_(u'Award Status'),
+                description=_(u'''
+                The current status of the award
+                '''),
+
+                default=[],
+                value_type=schema.Choice(
+                    vocabulary='ocds.AwardStatus'
+                    ),
+                required=False,
+                )
+
+    # Award date
+    award_date = schema.Date(
+        title=_(u'Award Date'),
+        description=_(u'''
+        The date of the contract award. This is usually the date on
+        which a decision to award was made.
+        '''),
+        required=False,)
+
+    # Value
+
+    award_amount = schema.Float(
+            title=_(u'Contract value'),
+            description=_(u'''
+            The total value of this award. In the case of a framework
+            contract this may be the total estimated lifetime value, or
+            maximum value, of the agreement. There may be more than one
+            award per procurement. A negative value indicates that the
+            award may involve payments from the supplier to the buyer
+            (commonly used in concession contracts).
+                '''),
+            required=False,
+            )
+
+    directives.widget(award_currency=SelectFieldWidget)
+    award_currency = schema.Choice(
+            title=u'Contract Value Currency',
+            description=u'Currency of the contract amount',
+            required=False,
+            vocabulary='collective.vocabularies.iso.currencies',
+            )
+
+    # Value
+
+    award_amount = schema.Float(
+            title=_(u'Contract value'),
+            description=_(u'''
+            The total value of this award. In the case of a framework
+            contract this may be the total estimated lifetime value, or
+            maximum value, of the agreement. There may be more than one
+            award per procurement. A negative value indicates that the
+            award may involve payments from the supplier to the buyer
+            (commonly used in concession contracts).
+                '''),
+            required=False,
+            )
+
+    directives.widget(award_currency=SelectFieldWidget)
+    award_currency = schema.Choice(
+            title=u'Contract Value Currency',
+            description=u'Currency of the contract amount',
+            required=False,
+            vocabulary='collective.vocabularies.iso.currencies',
+            )
+
+    # Contract period
+    
+    # startDate
+    contractPeriod_startDate = schema.Date(
+        title=_(u'Contract Start Date'),
+        description=_(u'''
+        The start date for the period. When known, a precise start date
+        must always be provided.
+        '''),
+        required=False,)
+
+    # endDate
+    contractPeriod_endDate = schema.Date(
+        title=_(u'Contract End Date'),
+        description=_(u'''
+        The end date for the period. When known, a precise end date must
+        always be provided.
+        '''),
+        required=False,)
+
+    # period_maxExtentDate
+    contractPeriod_maxExtentDate = schema.Date(
+            title=_(u'Contract Maximum extent'),
+            description=_(u'''
+            The period cannot be extended beyond this date. This field
+            is optional, and can be used to express the maximum
+            available data for extension or renewal of this period.
+            '''),
+            required=False,)
+
 
     # directives.widget(level=RadioFieldWidget)
     # level = schema.Choice(
