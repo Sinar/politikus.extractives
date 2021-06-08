28cf1e5cc8cbf1095b828968da69f85d7c71c7a9
Khairil Yusof <khairil.yusof@gmail.com>
2021-06-08 16:23:03 +0800

    Add extrative viewlet for natural resources.
    Displays natural resources, extractive concession is related to

--- src/politikus/extractives/viewlets/natural_resource_extractive_concessions_viewlet.py
+++ src/politikus/extractives/viewlets/natural_resource_extractive_concessions_viewlet.py
@@ -1,15 +1,37 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
-
+from Acquisition import aq_inner
+from zope.component import getUtility
+from zope.intid.interfaces import IIntIds
+from zope.security import checkPermission
+from zc.relation.interfaces import ICatalog
+from zope.schema.interfaces import IVocabularyFactory
 
 class NaturalResourceExtractiveConcessionsViewlet(ViewletBase):
 
-    def update(self):
-        self.message = self.get_message()
+    def backrefs(self, attribute_name):
+        """
+        Return back references from source object on specified attribute_name
+        """
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
 
-    def get_message(self):
-        return u'My message'
+        source_object = self.context
+        result = []
+
+        for rel in catalog.findRelations(
+            dict(to_id=intids.getId(aq_inner(source_object)),
+                 from_attribute=attribute_name)
+              ):
+
+            obj = intids.queryObject(rel.from_id)
+
+            if obj is not None and checkPermission('zope2.View', obj):
+                if obj.portal_type == 'Extractive Concession':
+                    result.append(obj)
+
+        return result
 
     def render(self):
         return super(NaturalResourceExtractiveConcessionsViewlet, self).render()
